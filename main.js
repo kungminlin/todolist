0b16a093f544dfcdfc1e716441d88ec1111dcd72
Unknown <mml0816@gmail.com>
2018-05-19 17:31:28 -0700

    basic setup
    setting up core functionality

---
+++ main.js
@@ -0,0 +1,107 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow, addWindow;
+
+// Listen for app to be ready
+app.on('ready', function() {
+    // Create New Window
+    mainWindow = new BrowserWindow({});
+
+    // Load html file into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('close', function() {
+        app.quit();
+    });
+
+    // Build Menu From Template
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle Add Window
+function createAddWindow() {
+    // Create New Window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add To Do List Item'
+    });
+
+    // Load html file into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    addWindow.on('close', function() {
+        addWindow = null;
+    });
+}
+
+// Catch IPC Renderer Messages
+ipcMain.on('item:add', function(e, item) {
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
+
+// Create Menu Template
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// Check Operating System
+if (process.platform == 'darwin') {
+    menuTemplate.unshift({});
+}
+
+// Add Developer Tools (Temp)
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle Dev Tools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
